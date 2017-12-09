dae16031c9c398328802ea997daebd01ffef1f99
itwsgroupproject <33837526+itwsgroupproject@users.noreply.github.com>
2017-12-08 19:23:47 -0500

    Add files via upload

---
+++ search.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {      
+    $("#submit").click(function(){
+        //In here write an ajax that sends a get request 
+        $.ajax({
+            type: "GET",
+            url: "http://129.161.195.97:8080/LukesDonuts/OrderList?name=int",
+        
+
+        });
+    });
+});
+
+
+
+
+
+
+
